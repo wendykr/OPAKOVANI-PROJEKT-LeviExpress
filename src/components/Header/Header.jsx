649eae6d7371a969635069fe431e12abf79224f0
Vendula Krajíčková <vendysacek@seznam.cz>
2024-04-03 20:46:00 +0200

    change anchor to Link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
 
@@ -7,7 +8,7 @@ export const Header = () => {
     <header>
       <div className="container">
         <h1 className="site-title">
-          <a href="/">LeviExpress</a>
+          <Link to="/">LeviExpress</Link>
         </h1>
       </div>
     </header>
